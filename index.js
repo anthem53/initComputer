567ad5a4817af491bf0b51cc697f1cc4cb208556
LeeJihyeon <anthem53@postech.ac.kr>
2024-09-16 19:30:38 +0900

    feat. 팀뷰어 제거, 구글 원격 추가 파일질라 추가

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ list = {
     ,"FastStone (이미지뷰어)" : "https://www.faststone.org/"
     ,"다음팟 플레이어 (구버전)" : "https://drive.google.com/file/d/1c6a7oZ-9tagsOh1Qc7oQziI6FLC-66FA/view?usp=sharing"
     , "스팀"  : "https://store.steampowered.com/about/"
-    , "팀뷰어" : "https://www.teamviewer.com/ko/download/free-download-with-license-options/" 
     , "Git Bash" : "https://git-scm.com/downloads" 
+    , "구글 원격 프로그램(주소창 메시지 확인)" : "./index.html?message=구글 원격 프로그램 다운받으셈. 팀뷰어 시간 제한 있어서 별로임."
+    , "파일질라" : "https://www.filezilla.kr/"
     ,"깃 초기 실행시 실행해야할 명령어" : "./gitCommand.html"
 }
 
